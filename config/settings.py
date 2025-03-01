2d0dbe1ffe318c5b8886203cb7930ac8a09f2e96
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2025-03-01 14:14:18 +0530

    updated django settings file env variable accessing (testing)

--- config/settings.py
+++ config/settings.py
@@ -196,7 +196,7 @@ cred = credentials.Certificate({
   "type": "service_account",
   "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
   "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
-  "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+  "private_key": str(os.environ.get("FIREBASE_PRIVATE_KEY")).replace('\\n', '\n'),
   "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
   "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
   "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
