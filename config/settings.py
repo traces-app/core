5c7c22364a23e15eaf761048c9650a6a77042dfe
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2025-03-01 14:20:28 +0530

    updated django settings file env variable accessing (testing)

--- config/settings.py
+++ config/settings.py
@@ -194,16 +194,16 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 cred = credentials.Certificate({
   "type": "service_account",
-  "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
-  "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+  "project_id": "traces-1a3f2",
+  "private_key_id": "535481797f801197fe7f7a0097444f2fbd50f76c",
   "private_key": str(os.environ.get("FIREBASE_PRIVATE_KEY")).replace('\\n', '\n'),
   "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
-  "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
-  "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
-  "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
-  "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+  "client_id": "112151634559767211133",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
   "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL"),
-  "universe_domain": os.environ.get("FIREBASE_UNIVERSE_DOMAIN"),
+  "universe_domain": "googleapis.com",
 })
 
 firebase_admin.initialize_app(cred)
